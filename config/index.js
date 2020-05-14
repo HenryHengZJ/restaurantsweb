c437ef7985588abee6089e25a538e5ecf49ac64b
HenryHengZJ <hzj94@hotmail.com>
2020-05-14 18:02:14 +0100

    Fixed config file NODE_DEV confusion -5th commit

--- config/index.js
+++ config/index.js
@@ -1,7 +1,8 @@
+
 import getConfig from 'next/config'
 
 const { publicRuntimeConfig } = getConfig()
-const { NODE_ENV, STIRPE_CLIENT_KEY } = publicRuntimeConfig
+const { NODE_DEV, STIRPE_CLIENT_KEY } = publicRuntimeConfig
 
-export const server = NODE_ENV === 'development' ? 'http://localhost:5000' : 'https://foodiebee.eu';
+export const server = NODE_DEV === 'development' ? 'http://localhost:5000' : 'https://restaurantsweb.herokuapp.com';
 export const STIRPE_KEY=STIRPE_CLIENT_KEY;
